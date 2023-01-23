b4c1cdf77ed576a540f495a1ef5f6d7b65795bac
Ulaş Özen <110843261+youlasozen@users.noreply.github.com>
2023-01-23 23:41:59 +0300

    States of America Game
    States of America Game

---
+++ game/main.py
@@ -0,0 +1,48 @@
+import turtle
+import pandas
+
+
+screen= turtle.Screen()
+screen.title("USA Game")
+image="blank_states_img.gif"
+turtle.addshape(image)
+
+
+turtle.shape(image)
+data=pandas.read_csv("50_states.csv")
+
+'''
+def get_mouse_click_coor(x,y):
+    print(x,y)
+
+turtle.onscreenclick(get_mouse_click_coor)
+turtle.mainloop()
+'''
+
+all_states=data.state.to_list()
+guessed_states=[]
+
+while len(guessed_states)<50:
+    answer_state=screen.textinput(title=f"{len(guessed_states)}/50 States Correct", prompt="What's another state's name?").title()
+    print(answer_state)
+
+    if answer_state == "Exit":
+        missing_states=[]
+        for state in all_states :
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t=turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data=data[data.state==answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+
+
